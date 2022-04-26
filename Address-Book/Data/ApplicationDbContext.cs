0cc8f85c32c09c8f49fec922849ca75000ad77c1
ChrisRivera276 <c.rivera07@outlook.com>
2022-04-26 17:12:55 -0400

    Add project files.

---
+++ Address-Book/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Address_Book.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Address_Book.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Address-Book/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace Address_Book.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+    }
+}
